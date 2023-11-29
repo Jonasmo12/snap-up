21086725ad4d85a1ba3c46bcd9ab22d01a51637b
Jonas Moloto <jonasmoloto@outlook.com>
2023-11-29 14:59:38 +0200

    fix double navbar error in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductComponent } from './product/product.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: NavbarComponent},
   {path: 'login', component: LoginComponent},
   {path: 'cart', component: CartComponent},
-  {path: 'product', component: ProductComponent}
+  {path: 'product', component: ProductComponent},
+  {path: '', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
