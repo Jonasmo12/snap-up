eab44c463a24738cf640700646af58222dec57c7
Jonas Moloto <jonasmoloto@outlook.com>
2023-11-29 13:29:11 +0200

    init product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../Product';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  url = "";
+  constructor(private cartService: CartService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert("You have added product to cart!!");
+  }
+
+  getProduct() {
+    return this.http.get(this.url)
+  }
+  
 
 }
+
