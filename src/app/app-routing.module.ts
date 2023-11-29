4f5ec4be573336dec5e1e17233026e5b555532dc
Jonas Moloto <jonasmoloto@outlook.com>
2023-11-29 14:09:08 +0200

    make routes to different components, add router link to app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NavbarComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'product', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
