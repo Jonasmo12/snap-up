6e66893b6ccf31a269ad8801b2a7345a5cdd3619
Puseletso-mollo <puseletsomollo26@gmail.com>
2023-11-29 13:43:50 +0200

    merge login form with existing main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    NavbarComponent,
+    LoginComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
