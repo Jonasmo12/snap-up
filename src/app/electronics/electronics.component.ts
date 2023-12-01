cf84868d486e40f2c347789baa582dd5288a982f
Samukelisiwe123 <samkelisiweshongwe69@gmail.com>
2023-12-01 13:04:07 +0200

    localstorage update

--- src/app/electronics/electronics.component.ts
+++ src/app/electronics/electronics.component.ts
@@ -14,13 +14,15 @@ export class ElectronicsComponent {
   public electronics$!: Observable<Product[]>;
 
   constructor(private apiService: ApiService, private cartService: CartService) {}
-
+productList:any[]=[];
 
   ngOnInit(): void {
       this.electronics$ = this.apiService.getElectronics();
   }
   addToCart(product: any) {
     this.cartService.addToCart(product);
+    this.productList.push(product);
+    localStorage.setItem('products', JSON.stringify(this.productList));
     console.log('Product added to cart:', product);
   }
 }
