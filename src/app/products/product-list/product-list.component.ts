319bc274113febf7a32371f8ace19c7eeea4d7b7
Jonas Moloto <jonasmoloto@outlook.com>
2023-12-10 20:14:48 +0200

    load all products onInit into clickedCategoryProducts$

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -16,12 +16,12 @@ export class ProductListComponent implements OnInit {
   public cartItems: Product[] = [];
   categorie$!: Observable<any[]>;
 
-  clickeCategoryProducts$!: Observable<Product[]>;
+  clickedCategoryProducts$!: Observable<Product[]>;
 
   selectedCategory?: CategoryNavComponent
 
   ngOnInit(): void {
-    this.products$ = this.apiService.getProducts();
+    this.clickedCategoryProducts$ = this.apiService.getProducts();
     this.categorie$ = this.apiService.getCategories();
   }
 
@@ -32,7 +32,7 @@ export class ProductListComponent implements OnInit {
     this.selectedCategory = category;
     console.log('selected Category: ', category)
     this.clickeCategoryProducts$ = this.apiService.getCategory(category).pipe(
-      //map(products => {return products.map(product => ({...product, quantity: 1}))}),
+      map(products => {return products.map(product => ({...product, quantity: 1}))}),
       tap(data => console.log('category products: ', data))
     )
   }
